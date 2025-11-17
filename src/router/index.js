69b95d329f008068bfe861366164d57d5588d0f3
Dexture Hwuy <nguyenhuuquy267@gmail.com>
2025-11-17 19:25:06 +0700

    ryt

--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,6 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    {
-      path: '/alone',
-      name: 'alone',
-      component: Benchmark
-    },
   ]
 })
 
